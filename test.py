212f0e7f9be8a2999fc031c6c9e1e9cde386fe50
Clayton Maxwell <clayton@c-maxwell.net>
2017-10-17 09:30:54 +1100

    removed some garbage

--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-import urllib2
-data = {"text": "Test"}
-req = urllib2.Request('http://10.1.1.4:9090/telegram')
-req.add_header('Content-Type', 'application/json')
-response = urllib2.urlopen(req, json.dumps(data))
